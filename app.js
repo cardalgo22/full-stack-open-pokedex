9dd145851d6657bc39cd6d48beb312f29ccc2363
Carlos Dalmau <carlosdalmau2002@gmail.com>
2025-01-16 17:30:05 +0100

    solved favicon

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.use(express.static("dist"))
+
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`server started on port ${PORT}`)
 });
